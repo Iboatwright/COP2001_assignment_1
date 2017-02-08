936ad090258964e55fa78a6a2e5b77f40fdcb744
antinym <ijbshield@gmail.com>
2017-02-07 16:40:47 -0800

    changes to facilitate better result output. fixed a couple errors in the exected results array.  TODO: test for negative values.

--- equationTests.cpp
+++ equationTests.cpp
@@ -1,4 +1,5 @@
-// developing an equation test driver
+// test driver for quadratic equation program
+// TODO:  Still need to test with negative values.
 #include <array>
 #include <string>
 #include <iostream>
@@ -27,7 +28,7 @@ void equationTests(){
 	ofstream outs;
 	outs.open(outfile);
 	// 1 = equal roots, 2 = imaginary roots, 3 = real roots
-	std::array<int, 95> expected {1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,2,2,2,1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,2,2,2,1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3};
+	std::array<int, 95> expected {1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,2,2,2,1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,2,2,2,1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,1,2,2,1,2,2,2,2,3,2,2,2,2,3,3,3,3,3,3,3,3,3,3};
 	std::array<double, 95> coefA {100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 	std::array<double, 95> coefB {0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,99,99,100,100,100,100,100,0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,99,99,100,100,100,100,100,0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,99,99,100,100,100,100,100,0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,99,99};
 	std::array<double, 95> coefC {0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100};
@@ -41,12 +42,15 @@ void equationTests(){
 			tmpRoot1 = (-coefB[i] + sqrt(tmpDisc))/(2*coefA[i]);
 			tmpRoot2 = (-coefB[i] - sqrt(tmpDisc))/(2*coefA[i]);
 			if (tmpRoot1 == tmpRoot2){ // equal roots
-				results[i] = (expected[i] == 1)?"passed":"failed - roots: " + std::to_string(tmpRoot1);
+				results[i] = (expected[i] == 1)?"passed ":"failed ";
+				results[i] += "- roots: " + std::to_string(tmpRoot1);
 			} else { // two different real roots
-				results[i] = (expected[i] == 3)?"passed":"failed root1: " + std::to_string(tmpRoot1) + ", root2: " + std::to_string(tmpRoot2);
+				results[i] = (expected[i] == 3)?"passed ":"failed ";
+				results[i] +=  "- root1: " + std::to_string(tmpRoot1) + ", root2: " + std::to_string(tmpRoot2);
 			}
 		} else { // imaginary roots
-			results[i] = (expected[i] == 2)?"passed":"failed - discriminant: " + std::to_string(tmpDisc);
+			results[i] = (expected[i] == 2)?"passed ":"failed ";
+			results[i] += "- discriminant: " + std::to_string(tmpDisc);
 		}
 		i++;
 	}
