6059a032ba4750bd0d8af89f2b34c276839e1bc5
antinym <ijbshield@gmail.com>
2017-02-08 10:21:56 -0500

    removed expected array and commented related output.  improved output readabillity. improved code readability.  removed redundant std prefixs. TODO - implement new test algorithms to use smaller test sets, test for polarity and better equality comparisons.

--- equationTests.cpp
+++ equationTests.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <iomanip>
 
 #define outfile "test_results.txt"
 
@@ -24,40 +25,64 @@ double discr(double a, double b, double c){
 	return (b*b-4*a*c);
 }
 
+/*  The coef arrays have a good range of values for a, b, and c.  The
+ *  first pass results are all based on positive inputs.  If I had time
+ *  I'd try to code this in vector format. I think that might be faster
+ *  and more efficient.
+ */
 void equationTests(){
 	ofstream outs;
 	outs.open(outfile);
-	// 1 = equal roots, 2 = imaginary roots, 3 = real roots
-	std::array<int, 95> expected {1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,2,2,2,1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,2,2,2,1,2,2,2,2,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,3,3,1,2,2,1,2,2,2,2,3,2,2,2,2,3,3,3,3,3,3,3,3,3,3};
-	std::array<double, 95> coefA {100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-	std::array<double, 95> coefB {0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,99,99,100,100,100,100,100,0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,99,99,100,100,100,100,100,0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,99,99,100,100,100,100,100,0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,99,99};
-	std::array<double, 95> coefC {0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100};
-	std::string results[95];
+	
+	array<double, 95> coefA {100,100,100,100,100,100,100,100,100,100,100,
+				             100,100,100,100,100,100,100,100,100,100,100,
+				             100,100,100,99,99,99,99,99,99,99,99,99,99,99,
+				             99,99,99,99,99,99,99,99,99,99,99,99,99,99,50,
+				             50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,
+				             50,50,50,50,50,50,50,50,50,1,1,1,1,1,1,1,1,1,
+				             1,1,1,1,1,1,1,1,1,1,1};
+	array<double, 95> coefB {0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,99,
+                             99,99,100,100,100,100,100,0,0,0,0,0,1,1,1,1,1,
+                             50,50,50,50,50,99,99,99,99,99,100,100,100,100,
+                             100,0,0,0,0,0,1,1,1,1,1,50,50,50,50,50,99,99,
+                             99,99,99,100,100,100,100,100,0,0,0,0,0,1,1,1,
+                             1,1,50,50,50,50,50,99,99,99,99,99};
+	array<double, 95> coefC {0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,
+                             50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,
+                             99,100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,
+                             100,0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,
+                             0,1,50,99,100,0,1,50,99,100,0,1,50,99,100,0,1,
+                             50,99,100,0,1,50,99,100,0,1,50,99,100};
+	string results[95];
+
 	double tmpDisc, tmpRoot1, tmpRoot2;
 	int i = 0;
 	
-	for (auto& e : expected) {
+	for (auto& e : coefA) {
 		tmpDisc = discr(coefA[i], coefB[i], coefC[i]);
 		if (tmpDisc > -1){ // real roots exist
 			tmpRoot1 = (-coefB[i] + sqrt(tmpDisc))/(2*coefA[i]);
 			tmpRoot2 = (-coefB[i] - sqrt(tmpDisc))/(2*coefA[i]);
 			if (tmpRoot1 == tmpRoot2){ // equal roots
 				//results[i] = (expected[i] == 1)?"passed ":"failed ";
-				results[i] += "- roots: " + std::to_string(tmpRoot1);
+				results[i] += "- roots: " + to_string(tmpRoot1);
 			} else { // two different real roots
 				//results[i] = (expected[i] == 3)?"passed ":"failed ";
-				results[i] +=  "- root1: " + std::to_string(tmpRoot1) + ", root2: " + std::to_string(tmpRoot2);
+				results[i] +=  "+ root1: " + to_string(tmpRoot1) +
+					", root2: " + to_string(tmpRoot2);
 			}
 		} else { // imaginary roots
 			//results[i] = (expected[i] == 2)?"passed ":"failed ";
-			results[i] += "- discriminant: " + std::to_string(tmpDisc);
+			results[i] += "~ discriminant: " + to_string(tmpDisc);
 		}
 		i++;
 	}
 	i=0;
 	for (auto& r : results) {
-		outs << "[" << (i+1) << "]  (a, b, c): (" << coefA[i] << ", " << _
-			coefB[i] << ", " << coefC[i] << ")\t" << r << endl;
+		outs << "[" <<setw(2)<<right << (i+1) << "]" <<setw(14)<<right 
+			<< "(a, b, c): (" <<setw(3)<<right << coefA[i] << ", "
+			<<setw(3)<<right << coefB[i] << ", " <<setw(3)<<right
+			<< coefC[i] << ") " << r << endl;
 		i++;
 	}
 	outs.close();
